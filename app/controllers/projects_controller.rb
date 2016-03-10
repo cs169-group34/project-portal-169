f43e56981c72f2b13a541f43be4c97b5d8695faf
Austin Tai <otwojoy@gmail.com>
2016-03-10 04:43:41 +0000

    created show view for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,10 @@ class ProjectsController < ApplicationController
       redirect_to projects_path
     end
     
+    def show
+       @project = Project.find(params[:id]) 
+    end
+    
     private
     
     def project_params
